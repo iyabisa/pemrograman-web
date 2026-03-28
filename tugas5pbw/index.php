b94185a6e649ad358a514e17ae6b0a96311cca76
devika <devikalorensa@gmail.com>
2026-03-29 00:27:30 +0700

    tugas5pbw

---
+++ tugas5pbw/index.php
@@ -0,0 +1,27 @@
+<?php
+define("PAJAK", 0.10);
+$barang = [
+    "Keyboard" => 150000,
+    "Mouse"    => 100000,
+    "Monitor"  => 1200000
+];
+$namaBarang = "Keyboard";
+$jumlahBeli = 2;
+$hargaSatuan = $barang[$namaBarang];
+$totalSebelumPajak = $hargaSatuan * $jumlahBeli;
+$pajak = $totalSebelumPajak * PAJAK;
+$totalBayar = $totalSebelumPajak + $pajak;
+echo "<h2>Perhitungan Total Pembelian (Dengan Array)
+</h2>";
+echo "Nama Barang: $namaBarang
+ <br>";
+echo "Harga Satuan: Rp " . number_format($hargaSatuan, 0, ',', '.') . 
+"<br>";
+echo "Jumlah Beli: $jumlahBeli 
+<br>";
+echo "Total Harga (Sebelum Pajak): Rp " . number_format($totalSebelumPajak, 0, ',', '.') . 
+"<br>";
+echo "Pajak (10%): Rp " . number_format($pajak, 0, ',', '.') . 
+"<br>";
+echo "<b>Total Bayar: Rp " . number_format($totalBayar, 0, ',', '.') .
+ "</b>";
